10e9ad4040db7b3f1f44f54c20857a71c44fee82
Yuri Coene <yuri01coene@gmail.com>
2026-05-16 10:00:47 -0400

    2° questão Wrappers NullPointerException

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main{
     public static void main(String[] args) {
     
-        int nota = 8;
-        Integer notaObj = nota; 
-        System.out.println(nota);
-        System.out.println(notaObj);
-        System.out.println(notaObj.getClass().getSimpleName());
+    
+        Integer X = null; 
+        System.out.println(X);
+        System.out.println(X.getClass().getSimpleName());
     
 
 
